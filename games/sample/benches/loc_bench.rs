a763bb664dd1965d153e90f3d4238218371800b8
Zannick <jokeserver@gmail.com>
2023-02-21 19:42:04 -0800

    Remove old 'call' bench.

--- games/sample/benches/loc_bench.rs
+++ games/sample/benches/loc_bench.rs
@@ -10,26 +10,7 @@ use libsample::context::Context;
 use libsample::graph;
 use libsample::items::Item;
 
-fn check_access_call(locs: &[graph::Location], ctx: &Context) -> i32 {
-    let mut i = 0;
-    for loc in locs {
-        if loc.can_access(ctx) {
-            i += 1
-        }
-    }
-    i
-}
-
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let locmap = graph::build_locations();
-    let mut ctx = Context::default();
-    ctx.collect(Item::Kokiri_Sword);
-    ctx.collect(Item::Buy_Deku_Stick_1);
-
-    c.bench_function("call", |b| {
-        b.iter(|| check_access_call(locmap.as_slice(), &ctx))
-    });
-
     let world = graph::World::new();
     let mut ctx = Context::new();
     world.skip_unused_items(&mut ctx);
