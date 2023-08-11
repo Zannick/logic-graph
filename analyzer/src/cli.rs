80f66852286dc0132a393d763c543440bc74f0db
Zannick <jokeserver@gmail.com>
2023-08-11 15:16:56 -0700

    Accept additional route files on commandline.

--- analyzer/src/cli.rs
+++ analyzer/src/cli.rs
@@ -3,9 +3,10 @@ use crate::context::*;
 use crate::route::*;
 use crate::world::*;
 use clap::{Parser, Subcommand};
+use std::fmt::Debug;
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 #[command(about = "Graph algorithm analysis", long_about = None)]
@@ -28,7 +29,6 @@ impl Cli {
 pub enum Commands {
     /// searches for a solution
     Search {
-        // TODO: move routes here into their own file(s)
         /// Text files with routes to start from
         #[arg(long, value_name = "FILE")]
         routes: Vec<PathBuf>,
@@ -42,10 +42,21 @@ pub enum Commands {
     },
 }
 
+pub fn read_from_file<P>(p: &P) -> String
+where
+    P: AsRef<Path> + Debug,
+{
+    let mut file = File::open(p).unwrap_or_else(|e| panic!("Couldn't open file {:?}: {:?}", p, e));
+    let mut rstr = String::new();
+    file.read_to_string(&mut rstr)
+        .unwrap_or_else(|e| panic!("Couldn't read from file {:?}: {:?}", p, e));
+    rstr
+}
+
 pub fn run<W, T, L>(
     world: &W,
     startctx: T,
-    routes: Vec<ContextWrapper<T>>,
+    mut route_ctxs: Vec<ContextWrapper<T>>,
     args: &Cli,
 ) -> Result<(), std::io::Error>
 where
@@ -54,16 +65,16 @@ where
     L: Location<Context = T>,
 {
     match &args.command {
-        Commands::Search { .. } => {
-            let search = Search::new(world, startctx, routes)?;
+        Commands::Search { routes } => {
+            route_ctxs.extend(routes.into_iter().map(|route| {
+                let rstr = read_from_file(route);
+                route_from_string(world, &startctx, &rstr).unwrap()
+            }));
+            let search = Search::new(world, startctx, route_ctxs)?;
             search.search()
         }
         Commands::Route { route, .. } => {
-            let mut file = File::open(&route)
-                .unwrap_or_else(|e| panic!("Couldn't open file {:?}: {:?}", route, e));
-            let mut rstr = String::new();
-            file.read_to_string(&mut rstr)
-                .unwrap_or_else(|e| panic!("Couldn't read from file {:?}: {:?}", route, e));
+            let rstr = read_from_file(route);
             println!(
                 "{}",
                 match debug_route(world, &startctx, &rstr) {
