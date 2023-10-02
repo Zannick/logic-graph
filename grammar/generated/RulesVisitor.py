a056cd11651c41cde553a73ed56262a18158afe7
Zannick <jokeserver@gmail.com>
2023-10-01 21:15:49 -0700

    Update antlr generated files.

--- grammar/generated/RulesVisitor.py
+++ grammar/generated/RulesVisitor.py
@@ -1,6 +1,6 @@
-# Generated from Rules.g4 by ANTLR 4.12.0
+# Generated from Rules.g4 by ANTLR 4.13.1
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .RulesParser import RulesParser
 else:
     from RulesParser import RulesParser
