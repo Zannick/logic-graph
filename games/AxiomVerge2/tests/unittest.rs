d09cec93ce01b55804f4c43f601c5372a7339bcd
Zannick <bswolf@google.com>
2024-01-17 23:26:30 -0800

    Add log file for unittests.

--- games/AxiomVerge2/tests/unittest.rs
+++ games/AxiomVerge2/tests/unittest.rs
@@ -11,5 +11,10 @@ fn main() {
     let mut routedir = dir.clone();
     routedir.pop();
     routedir.push("routes");
+    log4rs::init_file(
+        &PathBuf::from("settings/log4unittest.yml"),
+        Default::default(),
+    )
+    .unwrap();
     run_all_tests_in_dir::<graph::World, Context>(&dir, Some(&routedir));
 }
