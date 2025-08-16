77c6872ee535d9774f72d8f735f166861963aa8d
Zannick <jokeserver@gmail.com>
2025-08-15 20:41:07 -0700

    Separate indexes #222

--- migrations/1_create/up.sql
+++ migrations/1_create/up.sql
@@ -9,10 +9,15 @@ CREATE TABLE db_states (
     processed BOOLEAN NOT NULL DEFAULT false,
     queued BOOLEAN NOT NULL DEFAULT false,
     won BOOLEAN NOT NULL DEFAULT false,
-    hist BLOB,  -- serialized History (or empty)
+    hist BLOB,  -- serialized History
     prev BLOB,  -- serialized Context (which should be left serialized for prev lookup)
-    next_steps BLOB,  -- serialized Vec<History>> (exactly one step per child state)
+    next_steps BLOB,  -- serialized Vec<History> (exactly one step per child state)  TODO: do we really need this?
     PRIMARY KEY(raw_state(256)),
-    INDEX(progress, won, processed, time_since_visit, (elapsed + estimated_remaining))
+    INDEX(progress),
+    INDEX(processed),
+    INDEX(queued),
+    INDEX(won),
+    INDEX((elapsed + estimated_remaining)),
+    INDEX(time_since_visit)
 )
 DATA DIRECTORY = "/mnt/e/.mysql";  -- should be locally set by the user
